e156797fa38e81370bfcd3131270b5d9eae6bcd1
Will Price <pricew86@gmail.com>
2024-01-11 11:57:16 +0700

    style header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,29 @@
 import Link from 'next/link';
+import Logo from './Logo';
+
+const routes = [
+  { name: 'Home', path: '/' },
+  { name: 'All Events', path: '/events/all' },
+];
 
 export default function Header() {
   return (
-    <header>
-      <Link href='/'>Home</Link>
-      <Link href='/events/all'>All Events</Link>
+    <header className='flex justify-between items-center border-b border-white/10 h-14 px-3 sm:px-9'>
+      <Logo />
+      <nav>
+        <ul className='flex gap-x-6 text-sm'>
+          {routes.map((route) => (
+            <li key={route.path}>
+              <Link
+                href={route.path}
+                className='text-white/50 hover:text-white transition'
+              >
+                {route.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
     </header>
   );
 }
