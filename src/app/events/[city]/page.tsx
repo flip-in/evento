8ca0e48dac690bd7631914d9274aaed7a90547e8
Will Price <pricew86@gmail.com>
2024-01-12 10:28:35 +0700

    scrollbar syling

--- src/app/events/[city]/page.tsx
+++ src/app/events/[city]/page.tsx
@@ -1,5 +1,10 @@
+import H1 from '@/components/h1';
 import React from 'react';
 
 export default function EventsPage() {
-  return <div>EventsPage</div>;
+  return (
+    <main className='flex flex-col items-center py-24 px-[20px] min-h-[110vh]'>
+      <H1>Events in Austin</H1>
+    </main>
+  );
 }
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,14 +1,17 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Logo() {
   return (
     <div>
-      <Image
-        src='https://bytegrad.com/course-assets/react-nextjs/evento.png'
-        alt='EVENTO logo'
-        width={53}
-        height={12}
-      />
+      <Link href='/'>
+        <Image
+          src='https://bytegrad.com/course-assets/react-nextjs/evento.png'
+          alt='EVENTO logo'
+          width={53}
+          height={12}
+        />
+      </Link>
     </div>
   );
 }
