31b15129d2f7a04f51eb5bf452c03049ac460daa
Will Price <pricew86@gmail.com>
2024-01-18 15:52:18 +0700

    middleware for redirects

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,9 @@
-export function
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+  return NextResponse.redirect(new URL('/events/all', request.url))
+}
+
+export const config = {
+  matcher: ['/events', '/about']
+}
