cb6a73c55f68f8de4d7021691383117c0ccca833
Will Price <pricew86@gmail.com>
2024-02-02 12:06:34 +0800

    remove unused imports

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,5 @@
 import clsx, { ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
-import prisma from "./db"
-import { notFound } from "next/navigation"
-import { unstable_cache } from "next/cache"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
